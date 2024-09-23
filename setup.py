7850dbadb32e31edb753058fbbb1767f2eeca0b8
Orrnob Mahmud <53536803+OrrnobMahmud@users.noreply.github.com>
2024-09-23 19:40:47 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orrnob-drops-automation",
+    version="0.1.2",
+    description="A package for airdrop automation.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author="Orrnob Mahmud",
+    author_website="www.orrnobmahmud.com",
+    url="https://github.com/OrrnobMahmud/Orrnob-Drops-Automation",
+    packages=find_packages(),
+    install_requires=[
+        "colorama",
+        "requests",
+        "brotli",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
